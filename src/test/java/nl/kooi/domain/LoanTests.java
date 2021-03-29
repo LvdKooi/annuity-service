1a0ba4e92d6b6c80f5bfe415b6c37918f3d059cc
Laurens van der Kooi <lvvdkooi@hotmail.com>
2021-03-30 00:07:47 +0200

    loanValidation test

--- src/test/java/nl/kooi/domain/LoanTests.java
+++ src/test/java/nl/kooi/domain/LoanTests.java
@@ -3,6 +3,7 @@ package nl.kooi.domain;
 
 import org.junit.jupiter.api.Test;
 
+import javax.validation.Validation;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
@@ -13,7 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LoanTests {
-
 
     @Test
     public void incompleteLoanObjectTest() {
@@ -35,4 +35,21 @@ class LoanTests {
         assertThat(loan.getLoanTerm()).isEqualTo(Period.ofMonths(12));
     }
 
+    @Test
+    public void loanTermVsPeriodicityValidationsTest() {
+        var validator =
+                Validation.buildDefaultValidatorFactory().getValidator();
+
+        var loan = Loan.builder()
+                .initialLoan(BigDecimal.valueOf(1000.0))
+                .annualInterestPercentage(BigDecimal.ONE)
+                .loanTerm(Period.ofMonths(11))
+                .periodicity(SEMI_ANNUALLY)
+                .startdate(LocalDate.parse("2019-01-01"))
+                .timing(IMMEDIATE)
+                .build();
+       var violations = validator.validate(loan);
+       assertThat(violations.stream().findFirst()).isNotEmpty();
+       assertThat(violations.stream().findFirst().get().getMessage()).isEqualTo("The loan term doesn't match the periodicity.");
+    }
 }
