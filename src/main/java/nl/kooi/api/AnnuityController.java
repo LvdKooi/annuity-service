621cf1f090899e9ad1b4f75369437d17a51e5f6f
Laurens van der Kooi <lvvdkooi@hotmail.com>
2021-03-31 19:35:50 +0200

    Dto generation and refactoring ControllerExceptionHandler

--- src/main/java/nl/kooi/api/AnnuityController.java
+++ src/main/java/nl/kooi/api/AnnuityController.java
@@ -1,17 +1,17 @@
 package nl.kooi.api;
 
 
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nl.kooi.api.dto.LoanDto;
 import nl.kooi.api.dto.Mapper;
 import nl.kooi.api.dto.RepaymentScheduleDto;
 import nl.kooi.domain.RepaymentScheduleService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 
 /**
@@ -22,13 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-public class AnnuityController {
+public class AnnuityController implements nl.kooi.api.controller.AnnuityApi {
 
     private final RepaymentScheduleService repaymentScheduleService;
 
-    @PostMapping(path = "/repayment-schedule")
-    @ApiOperation("Generates a repayment schedule for an annuity loan.")
-    public RepaymentScheduleDto getRepaymentSchedule(@RequestBody LoanDto loan) {
-        return Mapper.map(repaymentScheduleService.getRepaymentScheduleForLoan(Mapper.map(loan)));
+    @Override
+    public ResponseEntity<RepaymentScheduleDto> getRepaymentSchedule(@Valid LoanDto loan) {
+        return ResponseEntity.ok(Mapper.map(repaymentScheduleService.getRepaymentScheduleForLoan(Mapper.map(loan))));
     }
 }
